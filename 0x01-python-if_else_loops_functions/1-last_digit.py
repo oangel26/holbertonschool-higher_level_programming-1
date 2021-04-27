2ed3a54d71caece12f9afef3d93b3341102cca4f
william <rodriguezborjawilliam@gmail.com>
2021-04-27 16:13:48 -0500

    change value to negative in print

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 msg = ""
-last_digit = (abs(number) % 10 * -1)
+last_digit = (abs(number) % 10) * -1
 
 if last_digit == 0:
     msg = "and is 0"
@@ -10,5 +10,4 @@ elif last_digit > 5:
     msg = "and is greater than 5"
 else:
     msg = "and is less than 6 and not 0"
-
 print("Last digit of %d is %d %s" % (number, last_digit, msg))
