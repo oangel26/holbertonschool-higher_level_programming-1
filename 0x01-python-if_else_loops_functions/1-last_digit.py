c6811d59f47366f8e9b0936f9c30ca8302680c36
william <rodriguezborjawilliam@gmail.com>
2021-04-27 16:49:29 -0500

    print a to z

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,6 +5,9 @@ msg = ""
 last_digit = (abs(number) % 10)
 if number < 0:
     last_digit = last_digit * -1
+
+if number < 0:
+    last_digit = last_digit * -1
 if last_digit == 0:
     msg = "and is 0"
 elif last_digit > 5:
@@ -12,3 +15,4 @@ elif last_digit > 5:
 elif last_digit < 6 and not 0:
     msg = "and is less than 6 and not 0"
 print("Last digit of %d is %d %s" % (number, last_digit, msg))
+
