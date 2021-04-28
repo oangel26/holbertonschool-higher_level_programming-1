417e2e3fff309590aa4f4bd4e4128ed1d7260064
william <rodriguezborjawilliam@gmail.com>
2021-04-28 18:10:26 -0500

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for i, c in enumerate(str):
+        ascii = ord(c)
+        if ascii >= 97 and ascii <= 122:
+            c = chr(ascii - 32)
+        print(c, end='')
+    print('')
