bd1ae108c5e5d62e7f509d15fcae1cd96e8bdf02
william <rodriguezborjawilliam@gmail.com>
2021-04-27 20:49:54 -0500

    print with q or e

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 99):
+for i in range(97, 123):
     if i != 101 and i != 113:
-        print("{} {}".format(i, hex(i)))
+        print("{}".format(chr(i)), end='')
