3bdcb1298fe92f722c44783d82e41edb638e0e73
william <rodriguezborjawilliam@gmail.com>
2021-04-28 00:40:56 -0500

    print combinations fix pep8

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 for i in range(1, 90):
-	first = i // 10
-	second = i % 10
-	if first == second:
-		continue
-	if i > 9 and first - second == 0:
-		continue
-	print("{}{}".format(first, second), end='')
-	if i != 89:
-		print(', ', end='')
+    first = i // 10
+    second = i % 10
+    if first == second:
+        continue
+    if i > 9 and first - second == 0:
+        continue
+    print("{}{}".format(first, second), end='')
+    if i != 89:
+        print(', ', end='')
 print('')
