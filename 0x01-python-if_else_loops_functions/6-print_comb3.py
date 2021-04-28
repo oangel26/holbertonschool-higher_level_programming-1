422f862fba16f08f9edbda967c2759fe353bc541
william <rodriguezborjawilliam@gmail.com>
2021-04-28 00:40:09 -0500

    print combinations

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-for i in range(1, 99):
-	first = i / 10
+for i in range(1, 90):
+	first = i // 10
 	second = i % 10
 	if first == second:
 		continue
-	if i > 9 and first - second:
+	if i > 9 and first - second == 0:
 		continue
-	print(first, second)
+	print("{}{}".format(first, second), end='')
 	if i != 89:
-		print(', ')
+		print(', ', end='')
 print('')
