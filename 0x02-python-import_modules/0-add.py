53fad4ecab2f0e9b60fc90ab6c756ec84c320d5c
william <rodriguezborjawilliam@gmail.com>
2021-04-30 18:30:58 -0500

    0x02 0 add with import

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,13 +1,7 @@
-#!/usr/bin/python3
-def add(a, b):
-    """My addition function
+from add_0 import add
 
-    Args:
-        a: first integer
-        b: second integer
+a = 1
+b = 2
 
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
-print("{:d} + {:d} = {:d}".format(1, 2, add(1, 2)))
+if __name__ == '__main__':
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
