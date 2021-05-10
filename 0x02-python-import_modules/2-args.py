47c19afada0341bbd6b71f121932be364cf43da1
william <rodriguezborjawilliam@gmail.com>
2021-05-10 15:50:07 -0500

    0x02 (2) message 0 args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,9 @@
 import sys
 if __name__ == "__main__":
     length = len(sys.argv)
-    print('{} {}:'.format(length - 1, 'arguments' if length > 2 else 'argument' ))
+    if length == 1:
+        print('0 arguments.')
+        exit(0)
+    print('{} {}:'.format(length - 1, 'arguments' if length > 2 else 'argument'))
     for i in range(1, length):
         print("{0}: {1}".format(i, sys.argv[i]))
-
