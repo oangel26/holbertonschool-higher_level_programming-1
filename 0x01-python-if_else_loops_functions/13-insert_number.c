b24a0cb4c0a2fb6b74395de4d9f2628da3de74e9
william <rodriguezborjawilliam@gmail.com>
2021-05-10 13:26:56 -0500

    0x01 (13) add in linked list sorted

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,6 +1,38 @@
 #include "lists.h"
 
+/**
+ * insert_node - insert node in sorted list
+ * @head: linked list
+ * @number: new data
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
-	return *head;
+	listint_t *node, *tmp = *head;
+
+	if (!head)/*edge case*/
+		return (NULL);
+
+	node = malloc(sizeof(listint_t));
+	if(!node)
+		return (NULL);
+
+	node->n = number;
+
+	if (!*head) /*add first node */
+	{
+		*head = node;
+		return (node);
+	}
+
+	while (tmp->next) /* add in sorted */
+	{
+		if (number <= tmp->next->n)
+		{
+						node->next = tmp->next;
+						tmp->next = node;
+						return (node);
+		}
+		tmp= tmp->next;
+	}
+	return (node);
 }
