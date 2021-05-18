38f723d12da9ac634b49e4ce5194e9eb70debdea
william <rodriguezborjawilliam@gmail.com>
2021-05-18 12:42:59 -0500

    0x03-python-data_structures(3)format with pep8

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     if not my_list:
-        return 
+        return
     reversed_list = my_list[:]
     reversed_list.reverse()
     for item in reversed_list:
