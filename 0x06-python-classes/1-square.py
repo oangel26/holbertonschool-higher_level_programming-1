6f56ace1a773beb3c94be904ac9e6c1b10e585ae
william <rodriguezborjawilliam@gmail.com>
2021-05-18 17:40:52 -0500

    0x06-0x06-python-classes (1) Private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Square data.
+
+Square class allow all data and methods of the square
+
+.. _Google Python Style Guide:
+   http://google.github.io/styleguide/pyguide.html
+"""
+
+
+class Square:
+    """this is the empty class Square
+    Args: 
+
+    Attributes:
+    """
+    def __init__(self, size=3):
+        """Constructor
+        Args:
+            size (int): size of square `size`.
+        """
+        self.__size = size
