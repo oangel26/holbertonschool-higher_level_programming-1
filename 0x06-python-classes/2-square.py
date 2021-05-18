06a0182a942deac442dc374cb42d3527d7feebc8
william <rodriguezborjawilliam@gmail.com>
2021-05-18 17:54:41 -0500

    0x06-0x06-python-classes (2) Handling exceptions

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -19,5 +19,13 @@ class Square:
         """Constructor
         Args:
             size (int): size of square `size`.
+        Raises:
+        TypeError: The ``size`` size must be an integer.
+        ValueError: ``size`` must be >= 0.
+
         """
+        if type(size).__name__ != 'int':
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
         self.__size = size
