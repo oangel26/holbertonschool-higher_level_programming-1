90a79ca4ef89a99f978d4cefde98d7770f9e82df
william <rodriguezborjawilliam@gmail.com>
2021-05-18 21:00:40 -0500

    0x06-python-classes (6) allow size 0

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,7 +39,7 @@ class Square:
     def size(self, size):
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
-        elif size <= 0:
+        elif size < 0:
             raise ValueError('size must be >= 0')
         self.__size = size
 
