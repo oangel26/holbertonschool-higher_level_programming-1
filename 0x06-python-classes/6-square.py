652dc5d6978f9291e1011728c578b14aabb8c983
william <rodriguezborjawilliam@gmail.com>
2021-05-18 20:41:41 -0500

    0x06-python-classes (6) fix setter size >=0 edge case

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,7 +39,7 @@ class Square:
     def size(self, size):
         if type(size).__name__ != 'int':
             raise TypeError('size must be an integer')
-        elif size < 0:
+        elif size <= 0:
             raise ValueError('size must be >= 0')
         self.__size = size
 
