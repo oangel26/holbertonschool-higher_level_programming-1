02e6d6c183af6dc62df88a151322a32b0307d782
williamzborja <rodriguezborjawilliam@gmail.com>
2021-05-22 19:31:12 -0500

    0x08-python-more_classes (7) fix pep8 format

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -18,7 +18,6 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
-    
         Rectangle.number_of_instances += 1
 
     @property
