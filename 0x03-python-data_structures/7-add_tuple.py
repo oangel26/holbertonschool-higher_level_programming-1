597fdd4a19726e2e4cb84a0af8eb5063474f0235
williamzborja <rodriguezborjawilliam@gmail.com>
2021-05-24 16:14:56 -0500

    0x03-python-data-structures (7) sum tuple

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
 
-
-def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) + len(tuple_b) < 4:
+def validate_index(tuple, idx):
+    try:
+        return tuple[idx]
+    except:
         return 0
 
-    return tuple(tuple_a[0] + tuple_b[0], tuple_b[1], tuple_b[1])
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    n1 = validate_index(tuple_a, 0)
+    n2 = validate_index(tuple_b, 0)
+
+    n3 = validate_index(tuple_a, 1)
+    n4 = validate_index(tuple_b, 1)
+    return n1 + n2, n3 + n4
