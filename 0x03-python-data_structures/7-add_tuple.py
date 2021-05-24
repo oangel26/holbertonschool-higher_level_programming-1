0bd67e23b8b784a0d9d863dc5ee71bab4534448a
williamzborja <rodriguezborjawilliam@gmail.com>
2021-05-24 15:43:43 -0500

    0x03-python-data-structures (7) sum tuple

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) + len(tuple_b) <= 4:
+        return 0
+
+    return tuple(tuple_a[0] + tuple_b[0], tuple_b[1], tuple_b[1])
