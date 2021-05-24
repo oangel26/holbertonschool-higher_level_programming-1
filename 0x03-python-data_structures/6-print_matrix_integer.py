fd091d8a3e460e32a83526d32780ae271ff383ac
williamzborja <rodriguezborjawilliam@gmail.com>
2021-05-24 15:27:57 -0500

    0x03-python-data-structures (6) print matrix and use format

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 
 def print_matrix_integer(matrix=[[]]):
     if len(matrix) == 1:
-        print(''.format(None))
+        print('{:s}'.format(''))
         return
     for row in matrix:
         for col in row:
