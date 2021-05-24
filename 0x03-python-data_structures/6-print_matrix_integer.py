952af0e6dd988da837bcf14aa3d1812cdc7fe2d0
williamzborja <rodriguezborjawilliam@gmail.com>
2021-05-24 14:17:56 -0500

    0x03-python-data-structures (6) max in matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-
 def print_matrix_integer(matrix=[[]]):
     greatest = None
+    if len(matrix) == 1:
+        print('')
+        return
     for row in matrix:
-        max_n = max(matrix)
+        max_n = max(row)
         if greatest is None or max_n > greatest:
             greatest = max_n
-    return greatest
+    print("{}".format(greatest))
