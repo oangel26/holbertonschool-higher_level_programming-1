7c7f5bbe3f780dccd104dc1b67a72f456a427cba
williamzborja <rodriguezborjawilliam@gmail.com>
2021-05-25 14:27:05 -0500

    0x03-python-data-structures (11) fix malloc in palindrome array

--- 0x03-python-data_structures/13-main.c
+++ 0x03-python-data_structures/13-main.c
@@ -1,32 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "lists.h"
 
 /**
- * main - check space complexity
+ * main - check runtime of is_palindrome - not palindrome
  *
  * Return: Always 0.
  */
 int main(void)
 {
 	listint_t *head;
+	listint_t *current;
+	clock_t start;
+	clock_t end;
+	clock_t diff;
+	int i;
 
 	head = NULL;
-	add_nodeint_end(&head, 98);
-	add_nodeint_end(&head, 50);
-	add_nodeint_end(&head, 0);
-	add_nodeint_end(&head, 727);
-	add_nodeint_end(&head, 972);
-	add_nodeint_end(&head, 727);
-	add_nodeint_end(&head, 0);
-	add_nodeint_end(&head, 50);
-	add_nodeint_end(&head, 98);
-	print_listint(head);
+	for (i = 0; i < 1001; i++)
+		add_nodeint_end(&head, i);
+	for (i = 1000; i >= 0; i--)
+		add_nodeint_end(&head, i);
 
-	if (is_palindrome(&head) == 1)
-		printf("Linked list is a palindrome\n");
+	current = head;
+	for (i = 0; i < 727; i++)
+		current = current->next;
+	current->n = -98;
+
+	start = clock();
+
+	for (i = 0; i < 10; i++)
+		is_palindrome(&head);
+
+	end = clock();
+
+	diff = (double)(end - start) / 10;
+
+	if (diff > 80)
+		printf("Runtime too long\n");
 	else
-		printf("Linked list is not a palindrome\n");
+		printf("OK\n");
 
 	free_listint(head);
 
