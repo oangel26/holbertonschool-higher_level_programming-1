fd5e9a8432ab27c51f2d9deb7262d781e5275c1e
williamzborja <rodriguezborjawilliam@gmail.com>
2021-06-02 18:32:34 -0500

    0x0A-python-inheritance - (1) print in order

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,2 +1,24 @@
 #!/usr/bin/python3
 
+"""
+module
+classes
+    MyList
+"""
+
+
+class MyList(list):
+    """
+    class MyList
+    """
+
+    def print_sorted(self):
+        """
+            this function copy list and print sorted values
+            Args:
+                self: Mylist
+        """
+        copied = self.copy()
+        copied.sort()
+        print(copied)
+        return copied
