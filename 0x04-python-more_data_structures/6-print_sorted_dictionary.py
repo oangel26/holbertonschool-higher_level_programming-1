1c62957afafd523a4bc8abd1e74b0d447cc4e102
william andres rodriguez borja <2159@holbertonschool.com>
2021-06-22 17:59:34 -0500

    (0x04) (6) print sorted dictionary

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    '''print dict sorted by keys'''
+    for key in sorted(a_dictionary):
+        print("{}: {}".format(key, a_dictionary[key]))
