8f9b9e54b81b645dd7dd4e9229bc91654033efe6
william andres rodriguez borja <2159@holbertonschool.com>
2021-06-22 14:46:00 -0500

    matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-
-    new_matrix = [[]]
-    
-
-    map()
-        
+    return list(map(lambda row: list(map(lambda n: n ** 2, row)), matrix))
