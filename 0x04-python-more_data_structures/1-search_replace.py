1095da699517c0fa977484fde156631a2963023a
william andres rodriguez borja <2159@holbertonschool.com>
2021-06-22 16:58:50 -0500

    search and replace in list

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    ''' search and replace function '''
+    result = []
+    for item in my_list:
+        if item == search:
+            result.append(replace)
+        else:
+            result.append(item)
+    return result
