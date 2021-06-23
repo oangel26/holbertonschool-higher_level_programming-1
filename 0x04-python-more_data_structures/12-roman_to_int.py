368802de75721aa31db2faf5e278154a1cb63801
william andres rodriguez borja <2159@holbertonschool.com>
2021-06-23 13:46:14 -0500

    (0x04) (12) fix pep8

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -11,10 +11,8 @@ def roman_to_int(roman_string):
         return total
 
     for idx, letter in enumerate(roman_string):
-        is_limit = idx < len(roman_string) - 1
-
-        is_next_great = roman_nums[roman_string[idx + 1]] > roman_nums[letter]
-        if is_limit and is_next_great:
+        if idx < len(roman_string) - 1 \
+                and roman_nums[roman_string[idx + 1]] > roman_nums[letter]:
             total += roman_nums[letter]
         else:
             total -= roman_nums[letter]
