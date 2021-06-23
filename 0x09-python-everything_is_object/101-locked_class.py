3edb3aa6bedc80648292124d2c319ce7cebcce56
william andres rodriguez borja <2159@holbertonschool.com>
2021-06-23 15:10:32 -0500

    (0x09) (101) fix pep8

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -27,5 +27,5 @@ class LockedClass:
 
     def __getattribute__(self, attr):
         if attr != 'first_name':
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(
-                attr))
+            raise AttributeError("'LockedClass' object has no attribute '{}'"
+            .format(attr))
