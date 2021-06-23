d2df478736d131d279860ed553e4119379c2239e
william andres rodriguez borja <2159@holbertonschool.com>
2021-06-23 12:40:07 -0500

    (0x04) (10) return key with max value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,13 +4,15 @@
 def best_score(a_dictionary):
     ''' find max value in dictionary '''
     max_value = -1
+    max_key = None
 
     if not a_dictionary:
         return
     for k, v in a_dictionary.items():
         if max_value is None or v > max_value:
             max_value = v
+            max_key = k
 
     if max_value == -1:
         return
-    return max_value
+    return max_key
