2b66b1c05f6a147a18857c3c00443aa5b52bbabd
william andres rodriguez borja <2159@holbertonschool.com>
2021-06-23 13:04:49 -0500

    (0x04) (12) edge case string none or not string

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -7,6 +7,9 @@ def roman_to_int(roman_string):
                   'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
 
+    if roman_string is None or type(roman_string) is not str:
+        return total
+
     for letter in roman_string:
         total += roman_nums[letter]
 
