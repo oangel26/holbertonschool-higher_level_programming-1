32cd9c093717f9958cc62777e490727a238c7f1f
william andres rodriguez borja <2159@holbertonschool.com>
2021-06-22 19:40:33 -0500

    (0x04) (7) update dictionary

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,7 @@
 
 def print_sorted_dictionary(a_dictionary):
     '''print dict sorted by keys'''
+    if not a_dictionary:
+        return
     for key in sorted(a_dictionary):
         print("{}: {}".format(key, a_dictionary[key]))
