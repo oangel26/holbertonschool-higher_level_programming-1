ccfe4897a5b265bc20b40cfb3855c28006aefb2f
william andres rodriguez borja <2159@holbertonschool.com>
2021-06-23 14:45:00 -0500

    (0x09) (101) documetation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+
+""" locked module """
 class LockedClass:
+    """
+    Locked class
+    Args:
+
+    Attr:
+        first_name : str
+    """
     def first_name(self):
         return(self.first_name)
