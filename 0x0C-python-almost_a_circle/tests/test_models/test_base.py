d484fa8d85e5db01f7662ac152f46e5a6a51a62a
william andres rodriguez borja <2159@holbertonschool.com>
2021-06-23 15:36:32 -0500

    (0x0C) (0) add setUp and tearDown methods for test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -8,18 +8,12 @@ class TestBase(unittest.TestCase):
     ''' test base cases'''
 
     def setUp(self):
-        self.base = Base(None)
-
-    def tearDown(self) -> None:
-        self.base = None
         Base.reset()
 
-    def test_base_id(self):
-        ''' create with correct id'''
-        self.assertEqual(self.base.id, 1)
+    def test_1_0(self):
+        """Create new instances: check for id."""
 
-    def test_increment_id(self):
-        ''' increment id'''
-        self.assertEqual(self.base.id, 1)
-        self.base = Base(None)
-        self.assertEqual(self.base.id, 2)
+        b0 = Base()
+        self.assertEqual(b0.id, 1)
+        b1 = Base()
+        self.assertEqual(b1.id, 2)
