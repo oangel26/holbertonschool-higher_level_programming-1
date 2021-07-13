9bda27431e4edd58104966dcb304bfed4816838a
William Andres Rodriguez Borja <rodriguezborjawilliam@gmail.com>
2021-07-12 20:31:07 -0500

    0x0D SQL - task100 move to utf8 with alter table convert to

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,4 @@
 -- move to utf8
-CREATE TABLE IF NOT EXISTS first_table(
-    id int(11) DEFAULT NULL,
-    name varchar(256)
-    CHARACTER SET utf8
-    COLLATE=utf8mb4_unicode_ci
-)
+use hbtn_0c_0
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
