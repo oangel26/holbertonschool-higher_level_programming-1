4a7777a46f15a844cc0bb2d56de2f863aee11610
William Andres Rodriguez Borja <rodriguezborjawilliam@gmail.com>
2021-07-12 20:13:23 -0500

    0x0D SQL - task100 move to utf8

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,7 @@
+CREATE TABLE first_table(
+    id int(11) DEFAULT NULL,
+    name varchar(256)
+    COLLATE utf8mb4_unicode_ci DEFAULT NULL)
+    DEFAULT CHARSET=utf8mb4
+    COLLATE=utf8mb4_unicode_ci
+)
