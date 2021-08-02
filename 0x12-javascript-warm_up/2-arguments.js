4b72f44d8120910562f0620d020a945327ca586d
William Andres Rodriguez Borja <rodriguezborjawilliam@gmail.com>
2021-08-02 23:34:20 +0000

    0x12 - task 2: fix standard

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,8 @@
 const length = process.argv.slice(2).length;
 if (length === 0) {
   console.log('No argument');
-} else if (length === 1){
+} else if (length === 1) {
   console.log('Argument found');
-}else {
-  console.log('Arguments found');    
+} else {
+  console.log('Arguments found');
 }
-
