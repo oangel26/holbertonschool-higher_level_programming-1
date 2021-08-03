030c5638092a75aafb1632387c3d69aaaa7d70b9
William Andres Rodriguez Borja <rodriguezborjawilliam@gmail.com>
2021-08-03 03:42:37 +0000

    0x12 - task 6: concat print

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const  argv  = process.argv;
+
+const penultimate = argv.slice(2)
+  .sort((a, b) => b - a)
+  .filter((item, index, array) => array.indexOf(item) === index)[1];
+
+console.log(penultimate || 0);
