4beed1e3f60b9425b69666901c42d82f722a9996
William Andres Rodriguez Borja <rodriguezborjawilliam@gmail.com>
2021-08-03 00:02:52 +0000

    0x12 - task 3:

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,2 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
-if (args[0] === undefined) {
-  console.log('No argument');
-} else if (args[1] === undefined) {
-  console.log(args[0]);
-} else {
-  console.log(args.join(' '));
-}
+console.log(process.argv[2] ? process.argv[2] : 'No argument');
