0a95a02d2fcf19a43be32938d5eb8c7f8b59cf1a
William Andres Rodriguez Borja <rodriguezborjawilliam@gmail.com>
2021-08-03 03:37:12 +0000

    0x12 - task 6: concat print

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const  argv = process.argv;
+
+const number = parseInt(argv[2]);
+
+const factorial = number => {
+  if (number === 0) {
+    return (1);
+  }
+  return (number * factorial(number - 1));
+};
+
+if (isNaN(number)) {
+  console.log(1);
+} else {
+  console.log(factorial(number));
+}
